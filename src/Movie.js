3d7b1eb8dd8a27b43dcb65bf14a89068c9b4346d
dvsoo <qpzm2480@naver.com>
2018-10-17 15:08:09 +0900

    component state basic

--- src/Movie.js
+++ src/Movie.js
@@ -1,24 +1,31 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import './Movie.css';
 
 class Movie extends Component {
-    render(){
-        return(
-            <div>
-                <MoviePoster poster={this.props.poster}/>
-                <h1>{this.props.title}</h1>
-            </div>
-        );
-    }
+	static propTypes = {
+		title: PropTypes.string.isRequired,
+		poster: PropTypes.string.isRequired,
+	};
+
+	render() {
+		return (
+			<div>
+				<MoviePoster poster={this.props.poster} />
+				<h1>{this.props.title}</h1>
+			</div>
+		);
+	}
 }
 
 class MoviePoster extends Component {
-    render(){
-        return(
-            <img src={this.props.poster}/>
-        );
-    }
+	static propTypes = {
+		poster: PropTypes.string.isRequired,
+	};
+
+	render() {
+		return <img src={this.props.poster} alt="Movie Poster" />;
+	}
 }
 
 export default Movie;
-
